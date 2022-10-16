f072879c34c7f72be067cbce5d59e659c8eb268b
so yeon <alicesysy@swu.ac.kr>
2022-10-16 21:51:26 +0900

    [Feat] TrendResponse 모델 생성 (#3)

--- NetworkExample/NetworkExample/Global/NetworkModel/TrendResponse.swift
+++ NetworkExample/NetworkExample/Global/NetworkModel/TrendResponse.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+// MARK: - Trend Response
+
+struct TrendResponse: Codable {
+    let results: [TrendMedia]
+    let page, totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case results, page
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+
+struct TrendMedia: Codable {
+    let posterPath: String
+    let backdropPath: String
+    let originalTitle, title: String?
+    let id: Int
+    let releaseDate: String?
+    let voteAverage: Double
+    let adult: Bool
+    let overview: String
+    let genre: [Int]
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case originalTitle = "original_title"
+        case title
+        case id
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case adult, overview
+        case genre = "genre_ids"
+    }
+}
