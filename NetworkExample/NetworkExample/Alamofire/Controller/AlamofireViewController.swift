b0dcbc4e6e9d1f75876acbbb60c440436d654ff8
so yeon <alicesysy@swu.ac.kr>
2022-10-16 22:21:23 +0900

    [Feat] Alamofire로 Trend Media GET 서버연결 (#4)

--- NetworkExample/NetworkExample/Alamofire/Controller/AlamofireViewController.swift
+++ NetworkExample/NetworkExample/Alamofire/Controller/AlamofireViewController.swift
@@ -12,6 +12,35 @@ final class AlamofireViewController: BaseViewController {
     // MARK: - Network
     
     override func fetchMovieList() {
+        AlamofireTrendAPIManger.shared.fetchMovieList(type: "all", time: "week") { result in
+            switch result {
+            case .success(let result):
+                dump(result)
+                
+            case .requestErr(let result):
+                print(result)
+                
+            case .serverErr:
+                self.makeAlert(title: "알림", message: "서버 오류입니다. 잠시 후 다시 시도해주세요 :(")
+                
+            default :
+                print("ERROR")
+            }
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    private func makeAlert(title : String,
+                           message : String,
+                           okAction : ((UIAlertAction) -> Void)? = nil,
+                           completion : (() -> Void)? = nil) {
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
         
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        self.present(alertViewController, animated: true, completion: completion)
     }
 }
--- NetworkExample/NetworkExample/Alamofire/Network/APIManager/AlamofireTrendAPIManger.swift
+++ NetworkExample/NetworkExample/Alamofire/Network/APIManager/AlamofireTrendAPIManger.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+import Alamofire
+
+final class AlamofireTrendAPIManger {
+    
+    static let shared = AlamofireTrendAPIManger()
+    
+    private init() { }
+    
+    func fetchMovieList(type: String, time: String, completionHandler: @escaping (NetworkResult<Any>) -> Void) {
+        let url = EndPoint.trend.requestURL + "/\(type)" + "/\(time)" + "?api_key=\(APIKey.KEY)"
+        let header : HTTPHeaders = ["Content-Type": "application/json"]
+        let dataRequest = AF.request(url,
+                                     method: .get,
+                                     encoding: JSONEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { dataResponse in
+            switch dataResponse.result {
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else { return }
+                guard let value = dataResponse.value else { return }
+                
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completionHandler(networkResult)
+                
+            case .failure: completionHandler(.pathErr)
+            }
+        }
+    }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(TrendResponse.self, from: data) else { return .pathErr }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.results)
+        case 400:
+            return .requestErr("Bad Request")
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+}
