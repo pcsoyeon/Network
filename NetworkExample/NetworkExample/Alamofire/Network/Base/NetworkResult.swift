e02cc27dec8b9f4e6010d230e55f0e75d6a60466
so yeon <alicesysy@swu.ac.kr>
2022-10-16 21:55:31 +0900

    [Add] NetwortResult 파일 생성 (#3)

---
+++ NetworkExample/NetworkExample/Alamofire/Network/Base/NetworkResult.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkResult.swift
+//  NetworkExample
+//
+//  Created by 소연 on 2022/10/16.
+//
+
+import Foundation
+
+enum NetworkResult<T> {
+    case success(T)
+    case requestErr(T)
+    case pathErr
+    case serverErr
+    case networkFail
+}
